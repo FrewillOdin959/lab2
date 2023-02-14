5c20f3ebe2e82bff633157faee894ebd6303ffdd
FrewillOdin959 <124051999+FrewillOdin959@users.noreply.github.com>
2023-02-14 14:21:40 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const HomePage = () => (
+  <div>
+    <h1>hi!</h1>
+    <p>Numele meu este <strong>Ianos v</strong>.</p>
+    <p>UTM <strong>CR-211</strong>.</p>
+    <p>love ya</p>
+  </div>
+);
 
-export default App;
+export default HomePage;
